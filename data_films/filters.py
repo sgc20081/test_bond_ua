002c40c761afadbd98a88c31acfcdfe118363b42
sgc20081 <109870908+sgc20081@users.noreply.github.com>
2024-03-16 15:43:01 +0200

    Update filters.py

--- data_films/filters.py
+++ data_films/filters.py
@@ -2,6 +2,6 @@ import django_filters as dj_fil
 from .models import Movie
 
 class MovieFilter(dj_fil.FilterSet):
-    year = dj_fil.DateFilter(field_name='movie_date', input_formats=['%Y-%m-%d'])
+    year = dj_fil.DateFilter(field_name='movie_date', input_formats=['%Y-%m-%d']) # year - name of the GET parameter that will be used
     director = dj_fil.CharFilter(field_name='movie_director__director_name')
     actor = dj_fil.CharFilter(field_name='movie_actors__actor_name')
